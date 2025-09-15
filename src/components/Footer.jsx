22ac636ebb3b5bfc4d1dbe3ba18b78cf674deb94
Rajeev Kumar <thakurrajeev37@gmail.com>
2025-09-14 20:10:20 -0400

    Updated footer component

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,4 +1,4 @@
-import { observer } from "mobx-react-lite";
+import { observer } from "mobx-react";
 import { useStore } from "../stores/StoreContext.jsx";
 
 const Footer = observer(function Footer() {
